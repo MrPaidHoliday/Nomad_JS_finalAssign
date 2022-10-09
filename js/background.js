2a69cd97f35ffe1e14a9a3b7597bdedb9b35e3b8
MrPaidHoliday <100908830+MrPaidHoliday@users.noreply.github.com>
2022-10-09 22:43:13 +0900

    Update background.js

--- js/background.js
+++ js/background.js
@@ -1,5 +1,5 @@
 const images = [
-    "0.jpg", "1.jpg", "2.jpg", "3.jpg", "4.jpg", "5.jpg", "6.jpg", "7.webp", "8.webp"
+    "0.jpg", "1.jpg", "2.jpg", "3.jpg"
 ];
 
 const chosenImage = images[Math.floor(Math.random() * images.length)];
@@ -9,5 +9,4 @@ const bgImage = document.createElement("img");
 bgImage.src = `img/${chosenImage}`;
 
 // document.body.appendChild(bgImage);
-document.body.style.background = `url(img/${chosenImage}) no-repeat center center fixed; -webkit-background-size: cover; -moz-background-size: cover; -o-background-size: cover; background-size: cover;`;
-
+document.body.style.background = `url(img/${chosenImage}) no-repeat center center fixed`;
